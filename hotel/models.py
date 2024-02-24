4fded91627473084bab6ba312cb07393501494a2
IlyA1536 <i0977187143@gmailcom>
2024-02-24 12:34:50 +0200

    Create classes: Room and Booking.

---
+++ hotel/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Room(models.Model):
+    number = models.CharField(max_length = 63)
+    capacity = models.IntegerField()
+    location = models.TextField()
+    description = models.TextField()
+
+    def __str__(self):
+        return f"Room - №{self.number}"
+
+    class Meta:
+        verbose_name = "Room"
+        verbose_name_plural = "Rooms"
+        ordering = ["number"]
+
+
+class Booking(models.Model):
+    room = models.ForeignKey(Room, on_delete = models.CASCADE, related_name = "bookings")
+    user = models.ForeignKey(User, on_delete = models.CASCADE, related_name = "bookings")
+
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now = True)
+
+    def __str__(self):
+        return f"Booked - {self.room} - by {self.user}"
+
+    class Meta:
+        verbose_name = "Booking"
+        verbose_name_plural = "Bookings"
+        ordering = ["-end_time", "room"]
