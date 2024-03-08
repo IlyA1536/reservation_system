02021a021a54786d7cdc52473dfa82d277e5b878
IlyA1536 <i0977187143@gmailcom>
2024-03-08 13:10:55 +0200

    Create booking_detail, booking_form and room_detail

--- hotel/views.py
+++ hotel/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from hotel.models import *
+from django.http import HttpResponse
 
 
 def get_rooms_list(request):
@@ -15,7 +16,6 @@ def get_rooms_list(request):
         context,
     )
 
-
 def get_users_list(request):
     users = User.objects.all()
 
@@ -28,3 +28,52 @@ def get_users_list(request):
         "hotel/users_list.html",
         context,
     )
+
+def room_detail(request, pk:int):
+    room = Room.objects.all()
+
+    context = {
+        "room" : room
+    }
+
+    return render(
+        request,
+        "hotel/room_detail.html",
+        context,
+    )
+
+def booking_detail(request, pk:int):
+    booking = Booking.objects.all()
+
+    context = {
+        "booking" : booking
+    }
+
+    return render(
+        request,
+        "hotel/booking_detail.html",
+        context,
+    )
+
+def booking_form(request):
+    if request.method == 'GET':
+        return render(request, 'hotel/booking_form.html')
+    else:
+        room_number = request.POST.get('room_number')
+        start_time = request.POST.get('start_time')
+        end_time = request.POST.get('end_time')
+
+        try:
+            room = Room.objects.get(number = room_number)
+        except ValueError:
+            return HttpResponse("Wrong room number", status=400)
+        except Room.DoesNotExist:
+            return HttpResponse("Room does not exist", status=404)
+        booking = Booking.objects.create(
+            room = room,
+            user = request.user,
+            start_time = start_time,
+            end_time = end_time,
+        )
+
+        return redirect("booking_detail", pk = booking.id)
