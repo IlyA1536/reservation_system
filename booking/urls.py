79dd49e4fa57e23b64139b62875dfef2f3513173
IlyA1536 <i0977187143@gmailcom>
2024-03-24 20:47:48 +0200

    Add function with booking date (do not version)

--- booking/urls.py
+++ booking/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('rooms-list/', get_rooms_list, name = 'get_rooms_list'),
     path('users/', get_users_list, name = 'get_users_list'),
     path('room/<int:pk>', room_detail, name = 'room_detail'),
-    path('booking/<int:pk>', booking_detail, name = 'booking_detail'),
-    path('booking/', booking_form, name = 'booking_form'),
+    path('booking-detail/<int:pk>', booking_detail, name = 'booking_detail'),
+    path('booking/<int:pk>', booking_form, name = 'booking_form'),
 ]
