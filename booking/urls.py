40a4da0a67277e1b3d36c893c420fa7f74fa260a
IlyA1536 <i0977187143@gmailcom>
2024-03-24 21:20:59 +0200

    Working version

--- booking/urls.py
+++ booking/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('users/', get_users_list, name = 'get_users_list'),
     path('room/<int:pk>', room_detail, name = 'room_detail'),
     path('booking-detail/<int:pk>', booking_detail, name = 'booking_detail'),
-    path('booking/<int:pk>', booking_form, name = 'booking_form'),
+    path('booking/', booking_form, name = 'booking_form'),
 ]
